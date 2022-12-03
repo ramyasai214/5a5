e487a21aee53e87cd47f3c59baabbb9bc64877d0
chand <chand@MSI>
2022-12-03 19:44:14 +0530

    junit5a5

---
+++ src/test/java/junit5a5/a5/AppTest.java
@@ -0,0 +1,28 @@
+package junit5a5.a5;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+  
+	@Test
+    public void test() {
+
+        App p = new App();
+        assertEquals("Hello ramya", p.getMessage("ramya"));
+
+    }
+    @Test
+    public void testNameEmpty() {
+
+        App c = new App();
+        assertEquals("Please provide a name!", c.getMessage(" "));
+
+    }
+}
